84ce2555b0b41590cfefa72d2db9542049ec72f3
Sayan Das <121851652+CoderSayan1@users.noreply.github.com>
2023-09-22 00:16:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,123 @@
-let timer = 60;
-let score = 0;
-let hitrn = 0;
-
-function makeBubble() {
-  let clutter = "";
-
-  for (let i = 0; i <= 200; i++) {
-    let rn = Math.floor(Math.random() * 10);
-    clutter += `<div class="bubble">${rn}</div>`;
-  }
-
-  document.querySelector("#pbottom").innerHTML = clutter;
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
 }
 
-function runTimer(){
-    var timerint = setInterval(function(){
-        if(timer > 0){
-            timer--;
-            document.querySelector("#timerval").textContent = timer;
-        }
-        else{
-            clearInterval(timerint);
-            document.querySelector("#pbottom").innerHTML = `<h1>Game Over!!</h1>`
-            let mySound = new Audio('audio/audio.mp3');
-            mySound.play();
-        }
-    }, 1000);
+html, body{
+    height: 100%;
+    width: 100%;
 }
 
-function getHit(){
-    hitrn = Math.floor(Math.random() * 10);
-    document.querySelector("#hitval").textContent = hitrn;
+#main{
+    height: 100%;
+    width: 100%;
+    background-color: rgb(240, 223, 201);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
 }
 
-function getScore(){
-    score += 10;
-    document.querySelector("#scoreval").textContent = score;
+#panel{
+    height: 80%;
+    width: 80%;
+    margin-top: 30px;
+    background-color: white;
+    border-radius: 10px;
+    overflow: hidden;
+    box-shadow: 4.8px 9.6px 9.6px hsl(0deg 0% 0% / 0.35);
 }
 
-document.querySelector("#pbottom").addEventListener("click", function(dets){
-    let clickedNum = Number(dets.target.textContent)
-    // console.log(clickedNum)
-    if(hitrn === clickedNum){
-        getScore();
-        getHit();
-        makeBubble();
+#ptop{
+    height: 100px;
+    width: 100%;
+    background-color: rgb(205, 157, 98);
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0px 20%;
+}
+
+.elem{
+    display: flex;
+    align-items: center;
+    gap: 20px;
+}
+
+.box{
+    background-color: white;
+    padding: 10px 20px;
+    font-weight: 600;
+    font-size: 20px;
+    border-radius: 5px;
+}
+
+#pbottom{
+    height: calc(100% - 100px);
+    width: 100%;
+    /* background-color: brown; */
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-wrap: wrap;
+    gap: 10px;
+    padding: 10px;
+}
+
+.bubble{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 60px;
+    width: 60px;
+    border-radius: 50%;
+    background-color: rgb(184, 152, 214);
+    color: #fff;
+    font-size: 22px;
+    font-weight: 300;
+}
+.bubble:hover{
+    background-color: rgb(129, 91, 165);
+    cursor: pointer;
+}
+
+#btn{
+    margin: 20px;
+    padding: 1rem 1.25rem;
+    font-size: 22px;
+    font-weight: 550;
+    border-radius: 25px;
+    border: none;
+    background-color: rgb(145, 216, 198);
+}
+#btn:hover{
+    background-color: rgb(73, 169, 145);
+    box-shadow:  inset 250px 250px 0 rgb(67, 192, 194);
+    cursor: pointer;
+}
+
+@media only screen and (max-width: 786px){
+    #ptop{
+        padding: 0 10%;
     }
-})
-
-makeBubble();
-runTimer();
-getHit();
+}
+@media only screen and (max-width: 618px){
+    #ptop{
+        padding: 0 5%;
+    }
+    .elem{
+        gap: 15px;
+    }
+}
+@media only screen and (max-width: 521px){
+    .box{
+        padding: 10px 15px;
+        font-size: 18px;
+        border-radius: 5px;
+    }
+    .elem{
+        display: flex;
+        flex-direction: column;
+    }
+}
